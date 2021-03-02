a3da7a19e8721d995cdbbaa1b0a4d2dd91b77992
ChossyUK <chossymills@gmail.com>
2021-03-02 12:02:42 +0000

    Initial Commit

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace CedarWoodSoftware
+{
+	public class SceneLoader : MonoBehaviour
+	{
+        #region Variables
+        [SerializeField] string levelToLoad = "";
+        [SerializeField] int currentLevelIndex = 0;
+        #endregion
+
+        #region Unity Base Methods
+        void Start()
+		{
+            Invoke("LoadLevel", 3f);
+		}
+		#endregion
+
+		#region User Methods
+	    void LoadLevel()
+        {
+            SceneManager.LoadScene(levelToLoad);
+        }
+		#endregion
+	}
+}
